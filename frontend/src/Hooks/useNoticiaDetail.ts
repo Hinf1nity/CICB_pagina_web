dc2a5e839bc35a0e94b9a5c7805d146324b36058
Franco1331 <gian.riveros@ucb.edu.bo>
2025-10-31 18:52:32 -0400

    Hook, NoticiasDetail
    Se realizaron cambios para mejorar la responsividad y para interactuar con el backend

--- frontend/src/Hooks/useNoticiaDetail.ts
+++ frontend/src/Hooks/useNoticiaDetail.ts
@@ -4,22 +4,32 @@ import api from '../Api/kyClient';
 export interface Noticia{
     id: number;
     title: string;
-    excerpt: string;
     category: string;
     date: string;
     image: string;
+    content: string;
+    pdf?: string;
 }
+export function useNoticiaDetail(id?: string){
+    const [noticia, setNoticias] = useState<Noticia>({
+  id: 0,
+  title: "",
+  category: "",
+  date: "",
+  image: "",
+  content: "",
+  pdf: ""
+});
 
-export function useNoticias(){
-    const [noticias, setNoticias]=useState<Noticia[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchNoticias = async () => {
         try{
-            const data: Noticia[] = await api.get("noticias").json();
+            const data: Noticia = await api.get(`noticias/${id}`).json();
             setNoticias(data);
+            console.log(data);
         }catch(err) {
             setError("Error al cargar las noticias");
         }finally{
@@ -28,6 +38,6 @@ export function useNoticias(){
     };
     fetchNoticias(); }, []);
 
-  return { noticias, loading, error };
+  return { noticia, loading, error };
 
 }
