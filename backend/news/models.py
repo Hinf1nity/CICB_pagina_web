77b149fecb14c98c8836f372b2ce1e59d235d998
auzabang <auzabanguille@gmail.com>
2025-11-03 16:12:29 -0400

    feat: modified fields for the databases

--- backend/news/models.py
+++ backend/news/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     titulo = models.CharField(max_length=255)
     fecha_publicacion = models.DateTimeField(auto_now_add=True)
     categoria = models.CharField(max_length=255, null=True, blank=True)
+    resumen = models.TextField()
     descripcion = models.TextField()
     imagen = models.ForeignKey(Img, on_delete=models.CASCADE, null=True, blank=True)
     pdf = models.ForeignKey(PDF, on_delete=models.CASCADE, null=True, blank=True)
