911b31cb22c3054f493bbef58edef6c1dac3ae10
auzabang <auzabanguille@gmail.com>
2025-11-24 18:20:27 -0400

    implemented permission limitations

--- backend/users/views.py
+++ backend/users/views.py
@@ -98,6 +98,13 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class UserStatisticsView(APIView):
     def get_permissions(self):
