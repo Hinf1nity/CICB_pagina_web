ce3e0598f9ed43c576068c14ad326506dae46e3c
AuzaBanG <142273820+AuzaBanG@users.noreply.github.com>
2025-11-24 21:13:24 -0400

    Add custom permissions to CallViewSet

--- backend/calls/views.py
+++ backend/calls/views.py
@@ -5,3 +5,10 @@ from .serializers import CallSerializer
 class CallViewSet(viewsets.ModelViewSet):
     queryset = Call.objects.all()
     serializer_class = CallSerializer
+    
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
