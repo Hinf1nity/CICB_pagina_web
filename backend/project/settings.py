10c70537a079bdace35adb24215d2b203220001b
ElKatzchen <sesecastro.s@gmail.com>
2025-12-01 20:26:07 -0400

    jwt

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -147,6 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
