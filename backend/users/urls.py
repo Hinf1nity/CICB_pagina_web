70bd62139520ea150f92ee40b9cf0332672a5171
auzabang <auzabanguille@gmail.com>
2025-12-08 19:49:48 -0400

    Added global view lock, migrated stats from users/ to stats/ specified endpoints overall -> lists all jobs news and users, users -> user stats, growth -> growth stats

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, UserStatisticsView, UserGrowthView
+from .views import UserViewSet
 
 router = DefaultRouter()
 router.register(r'', UserViewSet)  # /api/users/
 
 urlpatterns = [
-    path('stats/', UserStatisticsView.as_view(), name='user_stats'),
-    path('stats/growth/', UserGrowthView.as_view(), name='user_growth'),
     path('', include(router.urls)),
 ]
